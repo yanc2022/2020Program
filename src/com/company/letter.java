6380f81cf402b1b7098ec27c2a36e8fbda8dfa8d
Christine <christineyan2003@gmail.com>
2020-06-09 11:08:16 -0500

    .

---
+++ src/com/company/letter.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class letter {
+    public static final int SIZE = 9;
+
+    public static void main(String args[]) {
+
+        top();
+        bottom();
+    }
+
+    public static void top() {
+        for (int line = SIZE; line >= 0; line--) {
+            char letter = 65;
+            for (byte count = 0; count <=line; count++) {
+                System.out.print((char) (letter + count) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void bottom() {
+        for (int line = 0; line <= SIZE; line++) {
+            char letter = 65;
+            for (byte count = 0; count <=line; count++) {
+                System.out.print((char) (letter + count) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
