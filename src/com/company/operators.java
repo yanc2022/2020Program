f34c67ec85d5c7fb452195dbcb458e8a58c7c0d7
Christine <christineyan2003@gmail.com>
2020-06-17 21:14:28 -0500

    .

---
+++ src/com/company/operators.java
@@ -0,0 +1,38 @@
+package com.company;
+//Christine Yan
+
+//declare variables of mealCost, tipPercent, and taxPercent
+//set up scanner
+//ask user to input mealCost, tipPercent, and taxPercent
+//find amount of tip by setting up equation
+//find amount of tax by setting up equation
+//find total by adding mealCost, tax, and tip all together
+//round the total by using Math.round
+//print out total
+
+import java.util.Scanner;
+public class operators {
+    public static void main(String[] args) {
+
+        double mealCost;
+        int tipPercent;
+        int taxPercent;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What is the cost of your meal?");
+        mealCost = scan.nextDouble();
+        System.out.println("How much is your tip percentage?");
+        tipPercent = scan.nextInt();
+        System.out.println("What is your tax rate?");
+        taxPercent = scan.nextInt();
+
+        double tip = mealCost * tipPercent/100;
+        double tax = mealCost * taxPercent/100;
+        double total = mealCost + tip + tax;
+        total = Math.round(total);
+
+        System.out.println("Your total is: " + total);
+
+    }
+}
